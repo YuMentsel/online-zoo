5256e6da6acb25b440486b2382cfad1cb0787cdd
Yu Mentsel <yu.mentsel@gmail.com>
2022-09-26 23:41:40 +0300

    init: start task

---
+++ pages/main/script.js
@@ -0,0 +1,9 @@
+
+
+// navLink = document.querySelectorAll('.nav__link');
+
+// navLink.forEach((item) => {
+//   item.addEventListener('click', () => {
+//     nav.classList.toggle('active');
+//   });
+// });
